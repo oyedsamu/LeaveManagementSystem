4e2d0051f1a695bd8b2ddbbf6d7474fd4de90726
Oyedele Samuel <oyedsamu@gmail.com>
2019-09-19 23:27:15 +0100

    Progress

--- public/forindex.js
+++ public/forindex.js
@@ -1,8 +1,31 @@
 alert("Welcome to the Leave Management System");
 
-$.ajax({
-    url: "http://localhost:3000/employees"
-    data: 
-    success: success,
-    dataType: dataType
-});
+// var users = [
+//     {
+//         username: "damolaademola@amazemedia.com",
+//         password: "babaLoleshe666"
+//     },
+//     {
+//         username: "paulokoye@amazemedia.com",
+//         password: "uniquePassword1"
+//     },
+//     {
+//         username: "paulokoye@amazemedia.com",
+//         password: "uniquePassword1"
+//     }
+// ];
+
+// function loginverify(){
+//     let username= document.getElementById("username").value;
+//     let password= document.getElementById("password").value;
+//     let dept= document.getElementById("dept").value
+
+//     for (let i=0; i<users.length, i++){
+//         if (dept=="HR" && username==users[2].username && password==users[2].password) {
+//             location.replace("/public/HR.html");
+//         } else if(dept==){
+
+//         }
+//     }
+// }
+
